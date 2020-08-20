ed4411189463143e758c31357dd79a3c80bab7b4
angonch <angonch@cs.washington.edu>
2020-08-20 11:05:03 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Anna Goncharenko on 8/20/20.
+//  Copyright © 2020 annagoncharenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Anna Goncharenko on 8/20/20.
+//  Copyright © 2020 annagoncharenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
